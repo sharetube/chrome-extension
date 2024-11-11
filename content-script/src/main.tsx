81fc9cfe593cb7a1212f51458d649f9f9849b490
Nick <nikitakitsunedev@gmail.com>
2024-11-12 02:47:03 +0300

    📦 NEW: Player update

--- content-script/src/main.tsx
+++ content-script/src/main.tsx
@@ -1,17 +1,14 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 // Global styles
 import "@shared/styles/global.css";
 
 // Utils
-import render from "@shared/lib/render";
 import log from "@shared/lib/log";
 import waitForElement from "@shared/utils/waitForElement";
 import waitAdEnd from "@shared/utils/video/waitAdEnd";
 import addEvents from "@shared/utils/video/addEvents";
-// Observe
-import initializeOverlay from "@shared/utils/initializeOverlay";
-import observeNewElements from "@shared/utils/observeNewElements";
 
 // Types
 import player from "@shared/types/video/player";
@@ -29,47 +26,10 @@ Promise.all([
 
 // Render playlist
 waitForElement("#secondary-inner")
-    .then(elem => render(elem, <Playlist />))
+    .then(elem => ReactDOM.render(<Playlist />, elem))
     .catch(error => log("Failed to render playlist", error));
 
-// Define the elements to observe
-const elementsToObserve = [
-    {
-        selector: "ytd-topbar-logo-renderer",
-        overlayClass: "st-logo-icon-container",
-        zIndex: 2000,
-    },
-    {
-        selector: "ytd-compact-video-renderer",
-        overlayClass: "st-compact-video-renderer",
-        zIndex: 2100,
-    },
-    {
-        selector: "ytd-comment-view-model",
-        overlayClass: "st-comment-view-model",
-        zIndex: 2000,
-    },
-    {
-        selector: "ytd-video-owner-renderer",
-        overlayClass: "st-video-owner-renderer",
-        zIndex: 2000,
-    },
-    {
-        selector: "ytm-shorts-lockup-view-model-v2",
-        overlayClass: "st-shorts-lockup-view-model",
-        zIndex: 2000,
-    },
-    {
-        selector: "yt-lockup-view-model",
-        overlayClass: "st-lockup-view-model",
-        zIndex: 2000,
-    },
-];
-
-// Initialize overlays for defined elements
-elementsToObserve.forEach(({ selector, overlayClass, zIndex }) => {
-    initializeOverlay(selector, overlayClass, zIndex);
-});
-
-// Observe new elements in the DOM
-observeNewElements(elementsToObserve);
+// Remove comments
+waitForElement("ytd-item-section-renderer")
+    .then(elem => ReactDOM.render(<></>, elem))
+    .catch(error => log("Failed to render comments", error));
