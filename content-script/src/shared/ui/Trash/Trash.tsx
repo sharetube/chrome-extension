631ad24402da6e7c1027db6eb9885682b9672fe5
Nick <nikitakitsunedev@gmail.com>
2024-11-11 04:25:39 +0300

    👌 IMPROVE: Prettier update

--- content-script/src/shared/ui/Trash/Trash.tsx
+++ content-script/src/shared/ui/Trash/Trash.tsx
@@ -10,7 +10,7 @@ const Trash = () => {
             width="24"
             focusable="false"
             aria-hidden="true"
-            className="fill-current pointer-events-none w-[100%] h-[100%]"
+            className="pointer-events-none h-[100%] w-[100%] fill-current"
         >
             <path d="M11 17H9V8h2v9zm4-9h-2v9h2V8zm4-4v1h-1v16H6V5H5V4h4V3h6v1h4zm-2 1H7v15h10V5z"></path>
         </svg>
