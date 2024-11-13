36b51d17fd6c74f9941066cad99d24baef01b8b6
Nick <nikitakitsunedev@gmail.com>
2024-11-13 20:38:53 +0300

    📦 NEW: Input component & validation video url

--- content-script/src/main.tsx
+++ content-script/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // Global styles
-import "@shared/styles/global.css";
+import "./app/global.css";
 
 // Utils
 import log from "@shared/lib/log";
@@ -13,8 +13,9 @@ import addEvents from "@shared/utils/video/addEvents";
 // Types
 import player from "@shared/types/video/player";
 
-// Components
-import Playlist from "@widgets/ui/Playlist/Playlist";
+// Widgets
+import Search from "@widgets/ui/Search/Search";
+import Panel from "@widgets/ui/Panel/Panel";
 
 // Add event listeners to player after the ad ends
 Promise.all([
@@ -24,12 +25,25 @@ Promise.all([
     .then(([elem]) => addEvents(elem as player))
     .catch(error => log("Failed to add events to player", error));
 
-// Render playlist
-waitForElement("#secondary-inner")
-    .then(elem => ReactDOM.render(<Playlist />, elem))
-    .catch(error => log("Failed to render playlist", error));
+// Render main panel
+waitForElement("#secondary")
+    .then(elem => ReactDOM.render(<Panel />, elem))
+    .catch(error => log("Failed to render main panel", error));
 
-// Remove comments
-waitForElement("ytd-item-section-renderer")
-    .then(elem => ReactDOM.render(<></>, elem))
-    .catch(error => log("Failed to render comments", error));
+// Render search
+waitForElement("#center")
+    .then(elem => ReactDOM.render(<Search />, elem))
+    .catch(error => log("Failed to render input", error));
+
+/**
+ * Remove unnecessary elements
+ */
+// Remove voice search button
+waitForElement("#voice-search-button", 10000, 1)
+    .then(elem => elem?.remove())
+    .catch(error => log("Failed to remove voice search button", error));
+
+// Because clip button must be removed
+waitForElement("#flexible-item-buttons", 10000, 1)
+    .then(elem => elem?.remove())
+    .catch(error => log("Failed to remove clip button", error));
