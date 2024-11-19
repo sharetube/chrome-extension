5927105123cf90e4935100f43cb776b47020e0ba
Nick <nikitakitsunedev@gmail.com>
2024-11-19 21:42:05 +0300

    📦 NEW: Room design && Popup

---
+++ content-script/src/shared/icons/Top/Top.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Icon from "../Icon/Icon";
+
+const Top: React.FC = () => {
+    return (
+        <Icon>
+            <path d="M7.41 15.4199L12 10.83L16.59 15.4199L18 13.9999L12 7.9999L6 13.9999L7.41 15.4199Z" />
+        </Icon>
+    );
+};
+
+export default Top;
