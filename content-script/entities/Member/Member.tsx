e0991022471b8428b28e977ed48d0f5e52aacc3e
Nick Shiro <nikitakitsunedev@gmail.com>
2024-12-20 21:30:15 +0300

    👌 IMPROVE: Add box shadow; update Popup component

--- content-script/entities/Member/Member.tsx
+++ content-script/entities/Member/Member.tsx
@@ -5,124 +5,130 @@ import BigShield from "@shared/ui/BigShield/BigShield";
 import MemberIcon from "@shared/ui/Member/Member";
 import Mute from "@shared/ui/Mute/Mute";
 import Shield from "@shared/ui/Shield/Shield";
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 import { memo } from "react";
 import { ExtensionMessageType } from "types/extensionMessage";
 import { Member as IMember } from "types/serverMessage";
 
 type MemberProps = IMember;
 
-const Member: React.FC<MemberProps> = ({
-    id,
-    username,
-    avatar_url,
-    color,
-    is_ready,
-    is_muted,
-    ...props
-}) => {
-    const { is_admin } = useAdmin();
-    const [menu, setMenu] = React.useState(false);
+const Member: React.FC<MemberProps> = memo(
+    ({ id, username, avatar_url, color, is_ready, is_muted, ...props }) => {
+        const { is_admin } = useAdmin();
+        const [menu, setMenu] = React.useState(false);
 
-    const handleClick = (e: MouseEvent) => {
-        if ((e.target as HTMLElement).classList.contains("st-member")) return;
-        setMenu(false);
-    };
-
-    useEffect(() => {
-        const handleDocumentClick = (e: MouseEvent) => handleClick(e);
-
-        if (menu) {
-            document.addEventListener("click", handleDocumentClick);
-        } else {
-            document.removeEventListener("click", handleDocumentClick);
-        }
-
-        return () => {
-            document.removeEventListener("click", handleDocumentClick);
+        const handleClick = (e: MouseEvent) => {
+            if ((e.target as HTMLElement).classList.contains("st-member")) return;
+            setMenu(false);
         };
-    }, [menu]);
 
-    const openMenu = () => {
-        if (!is_admin) return;
-        setMenu(true);
-    };
+        useEffect(() => {
+            const handleDocumentClick = (e: MouseEvent) => handleClick(e);
 
-    const promote = () => {
-        if (!is_admin) return;
-        setMenu(false);
-        ContentScriptMessagingClient.sendMessage(ExtensionMessageType.PROMOTE_USER, id);
-    };
+            if (menu) {
+                document.addEventListener("click", handleDocumentClick);
+            } else {
+                document.removeEventListener("click", handleDocumentClick);
+            }
 
-    const kick = () => {
-        if (!is_admin) return;
-        setMenu(false);
-        ContentScriptMessagingClient.sendMessage(ExtensionMessageType.REMOVE_MEMBER, id);
-    };
+            return () => {
+                document.removeEventListener("click", handleDocumentClick);
+            };
+        }, [menu]);
 
-    return (
-        <li
-            className={`relative flex items-center  ${is_ready ? "" : "opacity-100"} ${is_admin ? "hover:cursor-pointer" : "hover:cursor-default"}`}
-            onClick={openMenu}
-        >
-            <Avatar size="s" url={avatar_url} color={color} letter={username.slice(0, 1)} />
-            {/* Nickname */}
-            <p
-                className={`m-0 p-[0_0_0_8px] text-text-primary font-secondary leading-normal text-[1.25rem] font-medium ${is_ready ? "" : "animate-pulse"}`}
-                style={{ color: color }}
-            >
-                {username}
-            </p>
-            {/* Icons */}
-            <div className="flex items-center">
-                {props.is_admin && (
-                    <div
-                        className={`text-text-primary h-[14px] w-[12px] box-border m-[0_0_0_4px] ${is_ready ? "" : "animate-pulse"}`}
-                    >
-                        <Shield />
-                    </div>
-                )}
-                {is_muted && (
-                    <div
-                        className={`text-text-secondary h-[12px] w-[12px] box-border m-[0_-2px_0_4px] ${is_ready ? "" : "animate-pulse"}`}
-                    >
-                        <Mute />
-                    </div>
-                )}
-            </div>
-            {is_admin && menu && !props.is_admin && (
+        const openMenu = useCallback(() => {
+            if (!is_admin) return;
+            setMenu(true);
+        }, [is_admin]);
+
+        const promote = useCallback(() => {
+            if (!is_admin) return;
+            setMenu(false);
+            ContentScriptMessagingClient.sendMessage(ExtensionMessageType.PROMOTE_USER, id);
+        }, [is_admin, id]);
+
+        const kick = useCallback(() => {
+            if (!is_admin) return;
+            setMenu(false);
+            ContentScriptMessagingClient.sendMessage(ExtensionMessageType.REMOVE_MEMBER, id);
+        }, [is_admin, id]);
+
+        return (
+            <li className="relative">
                 <div
-                    className="st-member absolute top-[36px] left-0 w-[150px] rounded-lg bg-spec-menu-background z-[2300] p-[8px_0]"
-                    onClick={e => {
-                        e.stopPropagation();
-                    }}
+                    className={`flex items-center  ${is_admin ? "hover:cursor-pointer" : "hover:cursor-default"} ${is_ready ? "" : "animate-pulse"}`}
+                    onClick={openMenu}
                 >
-                    <button
-                        onClick={promote}
-                        className="p-[0_16px] m-0 flex border-none rounded-[8px] bg-spec-menu-background items-center w-[24px] h-[36px] hover:cursor-pointer"
+                    <Avatar size="s" url={avatar_url} color={color} letter={username.slice(0, 1)} />
+                    {/* Nickname */}
+                    <p
+                        className={`m-0 p-[0_0_0_8px] text-text-primary font-secondary leading-normal text-[1.25rem] font-medium`}
+                        style={{ color: color }}
                     >
-                        <div className="h-[24px] w-[24px] flex items-center justify-center box-border text-text-primary">
-                            <BigShield />
-                        </div>
-                        <p className="text-[14px] leading-[2rem] font-normal font-secondary text-text-primary m-[0_24px_0_16px]">
-                            Promote
-                        </p>
-                    </button>
-                    <button
-                        onClick={kick}
-                        className="p-[0_16px] m-0 flex border-none rounded-[8px] bg-spec-menu-background items-center w-[24px] h-[36px] hover:cursor-pointer"
-                    >
-                        <div className="h-[24px] w-[24px] flex items-center justify-center box-border text-text-primary">
-                            <MemberIcon />
-                        </div>
-                        <p className="text-[14px] leading-[2rem] font-normal font-secondary text-text-primary m-[0_24px_0_16px]">
-                            Kick
-                        </p>
-                    </button>
+                        {username}
+                    </p>
+                    {/* Icons */}
+                    <div className="flex items-center">
+                        {props.is_admin && (
+                            <div
+                                className={`text-text-primary h-[14px] w-[12px] box-border m-[0_0_0_4px]`}
+                            >
+                                <Shield />
+                            </div>
+                        )}
+                        {is_muted && (
+                            <div
+                                className={`text-text-secondary h-[12px] w-[12px] box-border m-[0_-2px_0_4px]`}
+                            >
+                                <Mute />
+                            </div>
+                        )}
+                    </div>
                 </div>
-            )}
-        </li>
-    );
-};
+                {is_admin && menu && !props.is_admin && (
+                    <div
+                        className="st-member absolute top-[36px] left-0 w-[150px] rounded-lg shadow-box-shadow bg-spec-menu-background z-[2300] p-[8px_0]"
+                        onClick={e => {
+                            e.stopPropagation();
+                        }}
+                    >
+                        <div className="hover:cursor-pointer hover:bg-spec-button-chip-background-hover">
+                            <button
+                                onClick={e => {
+                                    console.log("Promote button clicked");
+                                    promote();
+                                }}
+                                className="p-[0_16px] m-0 flex border-none rounded-[8px] bg-transparent items-center w-[24px] h-[36px] "
+                            >
+                                <div className="h-[24px] w-[24px] flex items-center justify-center box-border text-text-primary hover:cursor-pointer">
+                                    <BigShield />
+                                </div>
+                                <p className="text-[14px] leading-[2rem] font-normal font-secondary text-text-primary m-[0_24px_0_16px] hover:cursor-pointer">
+                                    Promote
+                                </p>
+                            </button>
+                        </div>
+                        <div className="hover:cursor-pointer hover:bg-spec-button-chip-background-hover">
+                            <button
+                                onClick={e => {
+                                    console.log("Kick button clicked");
+                                    kick();
+                                }}
+                                className="p-[0_16px] m-0 flex border-none rounded-[8px] bg-transparent items-center w-[24px] h-[36px] hover:cursor-pointer"
+                            >
+                                <div className="h-[24px] w-[24px] flex items-center justify-center box-border text-text-primary hover:cursor-pointer">
+                                    <MemberIcon />
+                                </div>
+                                <p className="text-[14px] leading-[2rem] font-normal font-secondary text-text-primary m-[0_24px_0_16px] hover:cursor-pointer">
+                                    Kick
+                                </p>
+                            </button>
+                        </div>
+                    </div>
+                )}
+            </li>
+        );
+    },
+);
 
 export default Member;
