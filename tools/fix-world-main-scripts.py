3da21448fd6ec023adc3212fb27d031042e82cfc
skewb1k <101928759+skewb1k@users.noreply.github.com>
2024-12-25 18:43:29 +0300

    some fix-world-main-scripts tool changes

--- tools/fix-world-main-scripts.py
+++ tools/fix-world-main-scripts.py
@@ -6,7 +6,7 @@ import re
 args = sys.argv
 
 if len(args) != 2:
-    print("Usage: fix-world-main-scripts.py <ext dir>")
+    print("Usage: fix-world-main-scripts.py <extension dir>")
     sys.exit(1)
 
 ext_dir = args[1]
@@ -19,6 +19,7 @@ with open(ext_dir + "/manifest.json", "r") as manifestFile:
         content_script = content_scripts[i]
 
         if content_script.get("world") == "MAIN":
+            # fixme: iterate through all files
             loader_file_path = ext_dir + "/" + content_script.get("js")[0]
 
             with open(loader_file_path, "r") as loader_file:
@@ -27,11 +28,12 @@ with open(ext_dir + "/manifest.json", "r") as manifestFile:
                 content_script_path_regex = r'chrome\.runtime\.getURL\("([^"]+)"\)'
                 match = re.search(content_script_path_regex, loader)
                 if not match:
-                    print("Could not find content script path in loader file")
+                    print("Could not find content script import in loader file")
                     sys.exit(1)
 
                 content_script_path = match.group(1)
-                manifest["content_scripts"][i]["js"][0] = content_script_path
+                manifest.get("content_scripts")[i].get("js")[0] = content_script_path
                 os.remove(loader_file_path)
+
     with open(ext_dir + "/manifest.json", "w") as manifestFile:
         json.dump(manifest, manifestFile, indent=2)
