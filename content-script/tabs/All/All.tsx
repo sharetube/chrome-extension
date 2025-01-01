69abbdf56a4b623b5f428bca7d637d17ced75d96
Nick Shiro <nikitakitsunedev@gmail.com>
2025-01-02 01:21:07 +0300

    Add context menu item for starting a room and update styles

--- content-script/tabs/All/All.tsx
+++ content-script/tabs/All/All.tsx
@@ -1,4 +1,5 @@
 import waitForElement from "@shared/lib/waitForElement";
+import ContextItem from "@widgets/ContextItem/ContextItem";
 import Popup from "@widgets/Popup/Popup";
 import ReactDOM from "react-dom";
 
@@ -9,4 +10,43 @@ waitForElement("#end")
         elem?.prepend(container);
         ReactDOM.render(<Popup />, container);
     })
-    .catch(error => console.error("Failed to render popup", error));
+    .catch(error => console.error("ST: Failed to render popup", error));
+
+// Modify context menus
+waitForElement("ytd-popup-container")
+    .then(elem => {
+        const container = document.createElement("div");
+        container.id = "st-context-menu";
+        container.style.minWidth = "149px";
+
+        ReactDOM.render(<ContextItem />, container);
+
+        const handlePopupMutation = (mutations: MutationRecord[], observer: MutationObserver) => {
+            mutations.forEach(mutation => {
+                mutation.addedNodes.forEach(node => {
+                    if (node instanceof HTMLElement && node.tagName === "TP-YT-IRON-DROPDOWN") {
+                        const listboxElem = node.querySelector("tp-yt-paper-listbox");
+
+                        listboxElem!.prepend(container);
+                        observer.disconnect();
+
+                        new MutationObserver(mutations => {
+                            mutations.forEach(mutation => {
+                                mutation.removedNodes.forEach(node => {
+                                    if (
+                                        node instanceof HTMLElement &&
+                                        node.id === "st-context-menu"
+                                    ) {
+                                        listboxElem?.prepend(container);
+                                    }
+                                });
+                            });
+                        }).observe(listboxElem!, { childList: true });
+                    }
+                });
+            });
+        };
+
+        new MutationObserver(handlePopupMutation).observe(elem!, { childList: true });
+    })
+    .catch(error => console.error("ST: Failed to select 'ytd-popup-container.ytd-app'", error));
